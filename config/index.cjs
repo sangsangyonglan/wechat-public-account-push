05ce44bec286fefbb911c7d2d51a5be4ab36222d
sangsangyonglan <101847371+sangsangyonglan@users.noreply.github.com>
2023-06-13 18:45:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,13 +19,13 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '苗苗',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'osYid6ZVCfR9zUMkVzXvBFO855ss',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'YVbGBq5AzPXv19AP73f54Q06ofmQyTHOZCgs4h-C05M',
+      useTemplateId: '9dPfQ5x2ATFe38MEvSmFPw93uvLXSMkzgPrJ68PUCDM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '07-07',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
