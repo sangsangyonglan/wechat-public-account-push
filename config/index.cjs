b8f6485a406403f69e577e2cd4f75dad6690b5cc
sangsangyonglan <101847371+sangsangyonglan@users.noreply.github.com>
2023-06-13 19:32:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'osYid6ZVCfR9zUMkVzXvBFO855ss',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'JDPrLz3PKSuEnZpxDCNC33BLuTwR9k9VaZ1L2lmdOuI',
+      useTemplateId: 'WNQ7fzRdvS2qwSziyGphaRYdk0Z7_1TwhrZzlhzstV4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-07',
       festivals: [
@@ -36,7 +36,7 @@ const USER_CONFIG = {
           type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '生日', year: '2024', date: '06-22',
+          type: '节日', name: '生日', year: '2001', date: '06-22',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
